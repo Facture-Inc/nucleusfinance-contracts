cd696cc35f010b28ec655a69097a9a7e4881ec56
Adwit Mittal <adwitmittal@gmail.com>
2023-06-06 13:52:58 +0530

    added helper functions and modifiers

--- contracts/V2/test-contracts/XChain.sol
+++ contracts/V2/test-contracts/XChain.sol
@@ -5,18 +5,21 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "../imports/LazerZero/lzApp/NonblockingLzApp.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
- * @custom:todo update lz endpoint, destChainId & meson contract add
- * @custom:todo update token name constructor
- * @custom:todo update check decimals
+ * @custom:todo write comments.
  */
 contract XChain is NonblockingLzApp, AccessControl, Pausable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
     /*//////////////////////////////////////////////////////////////
                                 INITIALIZATION
     //////////////////////////////////////////////////////////////*/
+
     bytes32 public constant MAINTAINER_ROLE = keccak256("MAINTAINER_ROLE");
     uint16 public feePercentageBIPS;
+    uint256 internal feesCollected;
     address public maintainer;
     uint16 internal destChainId;
     uint256 public data;
@@ -27,13 +30,69 @@ contract XChain is NonblockingLzApp, AccessControl, Pausable, ReentrancyGuard {
         address _maintainer
     ) NonblockingLzApp(_lzEndpoint) {
         maintainer = _maintainer;
+        destChainId = _dstChainId;
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(MAINTAINER_ROLE, _maintainer);
     }
 
     /*//////////////////////////////////////////////////////////////
+                            CUSTOM ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    error invalidAddress();
+    error invalidAccessControl(address _add);
+    error invalidBIPS();
+
+    /*//////////////////////////////////////////////////////////////
+                    HELPER FUNCTIONS AND MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+
+    modifier onlyMaintainer() {
+        require(
+            hasRole(MAINTAINER_ROLE, msg.sender),
+            "maintainer role required"
+        );
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "admin role required");
+        _;
+    }
+
+    function pause() external onlyMaintainer {
+        _pause();
+    }
+
+    function unpause() external onlyMaintainer {
+        _unpause();
+    }
+
+    function isInvalidAddress(address _address) internal view returns (bool) {
+        return _address == address(this) || _address == address(0);
+    }
+
+    function setVaultFees(uint16 _feePercentageInBIPS) external onlyAdmin {
+        if ((_feePercentageInBIPS > 100) || (_feePercentageInBIPS < 0))
+            revert invalidBIPS();
+        feePercentageBIPS = _feePercentageInBIPS;
+    }
+
+    function calculateVaultFees(uint256 amount) public view returns (uint256) {
+        return (amount * feePercentageBIPS) / 10000;
+    }
+
+    function withdrawFee(address _token) external onlyMaintainer {
+        if (isInvalidAddress(_token)) revert invalidAddress();
+        uint256 fees = feesCollected;
+        feesCollected = 0;
+        IERC20(_token).safeTransfer(msg.sender, fees);
+    }
+
+    /*//////////////////////////////////////////////////////////////
                             LAYER ZERO
     //////////////////////////////////////////////////////////////*/
+
     function trustAddress(
         address _otherContract
     ) external nonReentrant onlyAdmin {
@@ -88,7 +147,5 @@ contract XChain is NonblockingLzApp, AccessControl, Pausable, ReentrancyGuard {
                             USER FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
-    function withdraw() external nonReentrant {
-        
-        }
+    function withdraw() external nonReentrant {}
 }
